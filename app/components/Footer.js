856956e6747dad3e7f2ee95081f7678e3db343c3
monther123c <monthertamimi123@gmail.com>
2023-01-07 07:35:27 +0000

    darkMode

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer(){
     return (
-        <footer  className="py-5 bg-blue-700 text-center text-white">
-            mmtⒸ 2023 
+        <footer  className="py-5 bg-blue-600  dark:bg-blue-800 text-center text-white">
+           Ⓒ 2023 
         </footer>
     )
 }
